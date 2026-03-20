1e78fcfd6bb2efb7a986bf10f52b56304dce0536
Muerte <74092032+Muerte0-0@users.noreply.github.com>
2026-03-20 15:24:46 +0530

    Update Build-GLFW.lua

--- Build-GLFW.lua
+++ Build-GLFW.lua
@@ -9,6 +9,10 @@ files
 {
 	"include/GLFW/glfw3.h",
 	"include/GLFW/glfw3native.h",
+	
+	"src/internal.h",
+	"src/platform.h",
+	"src/mappings.h",
 	"src/context.c",
 	"src/init.c",
 	"src/input.c",
@@ -16,23 +20,37 @@ files
 	"src/platform.c",
 	"src/vulkan.c",
 	"src/window.c",
+	"src/null_platform.h",
+	"src/null_joystick.h",
+	"src/null_init.c",
+	
+	"src/null_monitor.c",
+	"src/null_window.c",
+	"src/null_joystick.c",
 }
 
 filter "system:windows"
 systemversion "latest"
 
 files {
+	-- Win32 platform
 	"src/win32_init.c",
 	"src/win32_joystick.c",
 	"src/win32_monitor.c",
 	"src/win32_time.c",
 	"src/win32_window.c",
-	"src/wgl_context.c"
+	"src/win32_thread.c",
+	"src/win32_module.c",
+	
+	-- Context backends
+	"src/wgl_context.c",
+	"src/egl_context.c",
+	"src/osmesa_context.c",
 }
 
 defines {
 	"_GLFW_WIN32",
-	"_CRT_SECURE_NO_WARNINGS"
+	"_CRT_SECURE_NO_WARNINGS",
 }
 
 links {
@@ -49,15 +67,6 @@ defines {
 }
 
 files {
-	-- Core
-	"src/context.c",
-	"src/init.c",
-	"src/input.c",
-	"src/monitor.c",
-	"src/platform.c",
-	"src/vulkan.c",
-	"src/window.c",
-
 	-- POSIX
 	"src/posix_module.c",
 	"src/posix_poll.c",
